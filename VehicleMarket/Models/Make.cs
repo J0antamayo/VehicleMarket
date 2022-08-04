f3b3121c4b114940add2bfc770aa86114516dcc6
Joan Tamayo <jhoantamayo1@gmail.com>
2022-08-04 16:10:28 -0500

    Add project files.

---
+++ VehicleMarket/Models/Make.cs
@@ -0,0 +1,10 @@
+﻿namespace VehicleMarket.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Make Make { get; set; }
+    }
+}
---
+++ VehicleMarket/Models/Modelcs.cs
@@ -0,0 +1,8 @@
+﻿namespace VehicleMarket.Models
+{
+    public class Modelcs
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
