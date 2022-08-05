c9826aadc634d30531bd70145ea95fd5cb9a4281
Joan Tamayo <jhoantamayo1@gmail.com>
2022-08-05 18:07:36 -0500

    update

---
+++ VehicleMarket/Controllers/MakeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleMarket.AppDBContext;
+
+namespace VehicleMarket.Controllers
+{
+    public class MakeController : Controller
+    {
+        private readonly VehicleMarketDbContext _vehicleMarketDb;
+        public MakeController(VehicleMarketDbContext vehicleMarketDb)
+        {
+            _vehicleMarketDb = vehicleMarketDb;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
