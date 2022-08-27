5cfb4ee004df47bb5ac7a69c3a0dc17d1c8ebb61
Joan Tamayo <jhoantamayo1@gmail.com>
2022-08-26 22:11:12 -0500

    - Identity Implementation

--- VehicleMarket/Models/Model.cs
+++ VehicleMarket/Models/Model.cs
@@ -8,7 +8,7 @@ namespace VehicleMarket.Models
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public Make Make { get; set; }
         [Required]
         [Display(Name = "Make")]
